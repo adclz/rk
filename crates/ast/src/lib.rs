34b7c5435115a1de1ba00f0e2fe256988d439320
adclz <clauzeladrien@gmail.com>
2025-05-17 17:20:44 +0200

    feat: ast library with parsers

---
+++ crates/ast/src/lib.rs
@@ -0,0 +1,12 @@
+#![recursion_limit = "256"]
+pub mod generated;
+use crate::generated::SourceFile;
+use auto_lsp::configure_parsers;
+
+configure_parsers!(
+    RK_PARSER,
+    "structured_text" => {
+        language: tree_sitter_rk::LANGUAGE,
+        ast_root: SourceFile
+    }
+);
