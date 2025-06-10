7b9c79d60a1f3064ec72e3b33bd6291f0ae05116
adclz <clauzeladrien@gmail.com>
2025-06-10 21:28:26 +0200

    feat: semantic tokens + scopes

--- crates/server/src/capabilties/semantic_tokens.rs
+++ crates/server/src/capabilties/semantic_tokens.rs
@@ -1,10 +1,18 @@
-use ast::generated::NamespaceDecl;
-use auto_lsp::{anyhow, core::{ast::AstNode, dispatch, semantic_tokens_builder::SemanticTokensBuilder}, default::db::{tracked::get_ast, BaseDatabase, File}, define_semantic_token_modifiers, define_semantic_token_types, lsp_types::{SemanticTokensParams, SemanticTokensResult}};
+use std::sync::LazyLock;
+
+use ast::generated::{ClassDecl, DataTypeDecl, FbDecl, FuncDecl, InterfaceDecl, NamespaceDecl};
+use auto_lsp::{anyhow, core::{ast::AstNode, dispatch, semantic_tokens_builder::SemanticTokensBuilder}, default::db::{tracked::get_ast, BaseDatabase, File}, define_semantic_token_modifiers, define_semantic_token_types, lsp_types::{self, SemanticTokenModifier, SemanticTokensParams, SemanticTokensResult}, tree_sitter::{self, StreamingIterator}};
 
 define_semantic_token_types![
     standard {
         NAMESPACE,
-        FUNCTION
+        FUNCTION,
+        CLASS,
+        INTERFACE,
+        TYPE,
+        VARIABLE,
+        KEYWORD,
+        COMMENT,
     }
 
     custom {
@@ -14,13 +22,61 @@ define_semantic_token_types![
 
 define_semantic_token_modifiers![
     standard {
-
+        DECLARATION,
+        MODIFICATION
     }
 
     custom {
-        (INTERNAL, "internal")
+        (INTERNAL, "internal"),
+        (VOID, "void"),
     }
 ];
+
+static HIGHLIGHT_QUERY: LazyLock<tree_sitter::Query> = LazyLock::new(|| {
+    tree_sitter::Query::new(
+        &tree_sitter_rk::LANGUAGE.into(),
+        r#"
+        [
+         "NAMESPACE"
+         "END_NAMESPACE"
+         "FUNCTION"
+         "END_FUNCTION"
+         "FUNCTION_BLOCK"
+         "END_FUNCTION_BLOCK"
+         "TYPE"
+         "END_TYPE"
+         "CLASS"
+         "END_CLASS"
+         "INTERFACE"
+         "END_INTERFACE"
+        ] @keyword
+
+        ["PUBLIC" "PROTECTED" "PRIVATE" "INTERNAL"] @keyword.modifier
+
+        (namespace_decl name: (_) @declaration.namespace)
+        (func_decl name: (_) @declaration.function)
+        (fb_decl name: (_) @declaration.function_block)
+        (class_decl name: (_) @declaration.class)
+
+        (comment) @comment
+        "#,
+    )
+    .unwrap()
+});
+
+
+#[derive(Default)]
+pub(crate) struct ModifierSet(pub(crate) u32);
+
+impl std::ops::BitOrAssign<SemanticTokenModifier> for ModifierSet {
+    fn bitor_assign(&mut self, rhs: SemanticTokenModifier) {
+        let idx = SUPPORTED_MODIFIERS
+            .iter()
+            .position(|it| it == &rhs)
+            .unwrap();
+        self.0 |= 1 << idx;
+    }
+}
 
 pub fn semantic_tokens_full(
     db: &impl BaseDatabase,
@@ -34,29 +90,56 @@ pub fn semantic_tokens_full(
 
     let mut builder = SemanticTokensBuilder::new("".into());
 
-    get_ast(db, file).iter().try_for_each(|node| {
-        dispatch!(
-            node.lower(),
-            [
-                NamespaceDecl => tokens(db, file, &mut builder)
-            ]
+    let doc = file.document(db);
+    let root_node = doc.tree.root_node();
+
+    let mut query_cursor = tree_sitter::QueryCursor::new();
+    let mut captures = query_cursor.captures(&HIGHLIGHT_QUERY, root_node, doc.texter.text.as_bytes());
+
+
+    while let Some((m, capture_index)) = captures.next() {
+        let capture = m.captures[*capture_index];
+        let range = capture.node.range();
+
+        let parse_captures = HIGHLIGHT_QUERY.capture_names()[capture.index as usize].split(".").collect::<Vec<_>>();
+
+        let mut modifiers = ModifierSet::default();
+        let token_idx = match parse_captures[0] {
+            "keyword" => {
+                if parse_captures.len() > 1 {
+                match parse_captures[1] {
+                    "modifier" => modifiers |= MODIFICATION,
+                    _ => {}
+                    }
+                }
+                SUPPORTED_TYPES.iter().position(|x| *x == KEYWORD).unwrap() as u32
+
+            },
+            "comment" => SUPPORTED_TYPES.iter().position(|x| *x == COMMENT).unwrap() as u32,
+            "declaration" => {
+                modifiers |= DECLARATION;
+                match parse_captures[1] {
+                    "namespace" => SUPPORTED_TYPES.iter().position(|x| *x == NAMESPACE).unwrap() as u32,
+                    "function" => SUPPORTED_TYPES.iter().position(|x| *x == FUNCTION).unwrap() as u32,
+                    "function_block" => SUPPORTED_TYPES.iter().position(|x| *x == FUNCTION).unwrap() as u32,
+                    "class" => SUPPORTED_TYPES.iter().position(|x| *x == CLASS).unwrap() as u32,
+                    "interface" => SUPPORTED_TYPES.iter().position(|x| *x == INTERFACE).unwrap() as u32,
+                    "type" => SUPPORTED_TYPES.iter().position(|x| *x == TYPE).unwrap() as u32,
+                    _ => continue,
+                }
+            }
+            _ => continue,
+        };
+
+        builder.push(
+            lsp_types::Range {
+                start: lsp_types::Position::new(range.start_point.row as u32, range.start_point.column as u32) ,
+                end: lsp_types::Position::new(range.end_point.row as u32, range.end_point.column as u32),
+            },
+            token_idx,
+            modifiers.0,
         );
-        anyhow::Ok(())
-    })?;
-    Ok(Some(SemanticTokensResult::Tokens(builder.build())))
-}
-
-trait Tokens {
-    fn tokens(&self, db: &impl BaseDatabase, file: File, builder: &mut SemanticTokensBuilder) -> anyhow::Result<()>;
-}
-
-impl Tokens for NamespaceDecl {
-    fn tokens(&self, _db: &impl BaseDatabase, _file: File, builder: &mut SemanticTokensBuilder) -> anyhow::Result<()> {
-        builder.push(self.name.get_lsp_range(), 
-        SUPPORTED_TYPES.iter().position(|x| *x == NAMESPACE).unwrap() as u32, 
-        if self.internal.is_some() { 
-            SUPPORTED_MODIFIERS.iter().position(|x| *x == INTERNAL).unwrap() as u32
-         } else { 0 });
-        Ok(())
     }
+
+    Ok(Some(SemanticTokensResult::Tokens(builder.build())))
 }
