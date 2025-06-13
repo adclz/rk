c7d4a25ac443f4a5384f27731e8e9d9387d8eff8
adclz <clauzeladrien@gmail.com>
2025-06-13 17:43:51 +0200

    feat(function): implement Function and Variable structs with input/output variable management

--- crates/db/src/hir/function.rs
+++ crates/db/src/hir/function.rs
@@ -1,2 +1,18 @@
+use rustc_hash::FxHashMap;
+
+use crate::solver::Ident;
+
 #[salsa::tracked(debug)]
-pub struct Function<'db> {}
+pub struct Function<'db> {
+    input_variables: Vec<Variable<'db>>,
+    output_variables: Vec<Variable<'db>>,
+    in_out_variables: Vec<Variable<'db>>,
+    temp_variables: Vec<Variable<'db>>,
+    external_variables: Vec<Variable<'db>>,
+    global_variables: Vec<Variable<'db>>,
+}
+
+#[salsa::tracked(debug)]
+pub struct Variable<'db> {
+    name: Ident,
+}
--- crates/db/src/parser/function.rs
+++ crates/db/src/parser/function.rs
@@ -1,4 +1,8 @@
+use std::ops::Deref;
+
+use ast::generated::FuncVarDecls;
 use auto_lsp::default::db::{BaseDatabase, File};
+use crate::hir::function::Variable;
 use crate::parser::Parse;
 use crate::hir;
 
@@ -6,7 +10,77 @@ impl<'db> Parse<'db> for ast::generated::FuncDecl {
     type Output = hir::function::Function<'db>;
 
     fn parse(&self, db: &'db dyn BaseDatabase, file: File) -> Self::Output {
-        let doc = file.document(db);
-        hir::function::Function::new(db)
+        //hir::function::Function::new(db)
+        todo!()
+    }
+}
+
+trait ParseVariable<'db> {
+    fn parse(&self, db: &'db dyn BaseDatabase, file: File) -> Vec<Variable<'db>>;
+}
+
+impl<'db> ParseVariable<'db> for ast::generated::FuncDecl {
+    fn parse(&self, db: &'db dyn BaseDatabase, file: File) -> Vec<Variable<'db>> {
+        type VariableDecl = ast::generated::FuncVarDecls_IoVarDecls_TempVarDecls;
+        self.variables.iter().for_each(|variable| {
+            match variable.deref() {
+                VariableDecl::FuncVarDecls(variable) => {
+                    match variable.children.deref() {
+                        ast::generated::ExternalVarDecls_VarDecls::VarDecls(variable) => {
+                            variable.children.iter().for_each(|variable| {
+                                
+                            });
+                        }
+                        ast::generated::ExternalVarDecls_VarDecls::ExternalVarDecls(variable) => {
+                            variable.children.iter().for_each(|variable| {
+                                
+                            });
+                        }
+                    }
+                }
+                VariableDecl::IoVarDecls(variable) => {
+                    match variable.children.deref() {
+                        ast::generated::InOutDecls_InputDecls_OutputDecls::InputDecls(variable) => {
+                            variable.children.iter().for_each(|variable| {
+
+                            });
+                        }
+                        ast::generated::InOutDecls_InputDecls_OutputDecls::InOutDecls(variable) => {
+                            variable.children.iter().for_each(|variable| {
+                                
+                            });
+                        }
+                        ast::generated::InOutDecls_InputDecls_OutputDecls::OutputDecls(variable) => {
+                            variable.children.iter().for_each(|variable| {
+                                
+                            });
+                        }
+                    }
+                }
+                VariableDecl::TempVarDecls(variable) => {
+                    variable.children.iter().for_each(|variable| {
+                        match variable.deref() {
+                            ast::generated::InterfaceVarDecl_RefVarDecl_VarDecl::InterfaceVarDecl(variable) => {
+                                variable.children.iter().for_each(|variable| {
+                                
+                                });
+                            }
+                            ast::generated::InterfaceVarDecl_RefVarDecl_VarDecl::RefVarDecl(variable) => {
+                                variable.children.iter().for_each(|variable| {
+                                
+                                });
+                            }
+                            ast::generated::InterfaceVarDecl_RefVarDecl_VarDecl::VarDecl(variable) => {
+                                variable.children.iter().for_each(|variable| {
+                                
+                                });
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        
+        vec![]
     }
 }
