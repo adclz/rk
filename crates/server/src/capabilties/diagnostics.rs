7052d2a95f685a870c0986faf1ee8d3d45cb5ca7
adclz <clauzeladrien@gmail.com>
2025-06-18 00:40:12 +0200

    feat(diagnostics): add workspace diagnostics

--- crates/server/src/capabilties/diagnostics.rs
+++ crates/server/src/capabilties/diagnostics.rs
@@ -1,10 +1,9 @@
 use auto_lsp::anyhow;
 use auto_lsp::default::db::BaseDatabase;
 use auto_lsp::lsp_types::{
-    DocumentDiagnosticParams, DocumentDiagnosticReport, DocumentDiagnosticReportResult,
-    FullDocumentDiagnosticReport, RelatedFullDocumentDiagnosticReport,
+    DocumentDiagnosticParams, DocumentDiagnosticReport,  WorkspaceFullDocumentDiagnosticReport, DocumentDiagnosticReportResult, FullDocumentDiagnosticReport, RelatedFullDocumentDiagnosticReport, WorkspaceDiagnosticParams, WorkspaceDiagnosticReport, WorkspaceDiagnosticReportResult, WorkspaceDocumentDiagnosticReport
 };
-use db::diagnostics::{get_diagnostics};
+use db::diagnostics::{cached_diagnostics};
 
 pub fn diagnostics(
     db: &impl BaseDatabase,
@@ -21,8 +20,41 @@ pub fn diagnostics(
             related_documents: None,
             full_document_diagnostic_report: FullDocumentDiagnosticReport {
                 result_id: None,
-                items: get_diagnostics(db, file).clone(),
+                items: cached_diagnostics(db, file).iter().map(|d| d.diagnostic.clone()).collect(),
             },
         }),
+    ))
+}
+
+
+
+pub fn workspace_diagnostics(
+    db: &impl BaseDatabase,
+    params: WorkspaceDiagnosticParams,
+) -> anyhow::Result<WorkspaceDiagnosticReportResult> {
+
+    let result: Vec<WorkspaceDocumentDiagnosticReport> = db
+        .get_files()
+        .iter()
+        .map(|file| {   
+            let file = *file;
+            let errors: Vec<auto_lsp::lsp_types::Diagnostic> = cached_diagnostics(db, file)
+                .iter()
+                .map(|d| d.into())
+                .collect();
+
+            WorkspaceDocumentDiagnosticReport::Full(WorkspaceFullDocumentDiagnosticReport {
+                version: None,
+                full_document_diagnostic_report: FullDocumentDiagnosticReport {
+                    result_id: None,
+                    items: errors,
+                },
+                uri: file.url(db).clone(),
+            })
+        })
+        .collect();
+    
+    Ok(WorkspaceDiagnosticReportResult::Report(
+        WorkspaceDiagnosticReport { items: result },
     ))
 }
