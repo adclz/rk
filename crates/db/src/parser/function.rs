57b3109ffeab3f7d6c511289647d4ed9ea6fdcc3
adclz <clauzeladrien@gmail.com>
2025-06-18 00:43:55 +0200

    feat(hir): WIP: function hir parser

--- crates/db/src/parser/function.rs
+++ crates/db/src/parser/function.rs
@@ -1,85 +1,166 @@
 use std::ops::Deref;
 
-use auto_lsp::default::db::{BaseDatabase, File};
-use crate::hir::function::Variable;
-use crate::parser::Parse;
 use crate::hir;
+use crate::hir::variable::{Array, BitString, Literal, PrimitiveKind, Variable, VariableKind};
+use crate::parser::Parse;
+use crate::ident::Ident;
+use ast::generated::FuncVariables;
+use auto_lsp::core::ast::AstNode;
+use auto_lsp::default::db::{BaseDatabase, File};
 
 impl<'db> Parse<'db> for ast::generated::FuncDecl {
     type Output = hir::function::Function<'db>;
 
-    fn parse(&self, _db: &'db dyn BaseDatabase, _file: File) -> Self::Output {
-        //hir::function::Function::new(db)
-        todo!()
+    fn parse(&self, db: &'db dyn BaseDatabase, file: File) -> Self::Output {
+        let input_variables = self.parse_variables(db, file);
+        hir::function::Function::new(db, input_variables, vec![], vec![], vec![], vec![], vec![])
     }
 }
 
 trait ParseVariable<'db> {
-    fn parse(&self, db: &'db dyn BaseDatabase, file: File) -> Vec<Variable<'db>>;
+    fn parse_variables(&self, db: &'db dyn BaseDatabase, file: File) -> Vec<Variable<'db>>;
+}
+
+trait ToVariable<'db> {
+    fn to_variables(&self, db: &'db dyn BaseDatabase, file: File) -> Variable<'db>;
+}
+
+trait ParseConstant<'db> {
+    fn p(&self, db: &'db dyn BaseDatabase, file: File);
+}
+
+impl<'db> ParseConstant<'db> for ast::generated::ConstantExpr {
+
+    fn p(&self, db: &'db dyn BaseDatabase, file: File) {
+        type Constant = ast::generated::BitStrLiteral_BoolLiteral_CharLiteral_NumericLiteral_TimeLiteral;
+
+        match self.children.children.deref() {
+            Constant::BoolLiteral(bool_literal) => {
+                Literal::Bool(Ident::from_node(db, file, bool_literal.value.deref()).unwrap());
+            },
+            Constant::BitStrLiteral(bit_str_literal) => {
+                match bit_str_literal.int.deref() {
+                    ast::generated::BinaryInt_HexInt_OctalInt_UnsignedInt::BinaryInt(binary_int) => {
+                        let bits = binary_int.children
+                            .iter()
+                            .map(|bit| Ident::from_node(db, file, bit.deref()).unwrap())
+                            .collect::<Vec<_>>();
+                        Literal::BitString(BitString::Binary(Ident::join(db, &bits)))
+                    },
+                    ast::generated::BinaryInt_HexInt_OctalInt_UnsignedInt::HexInt(hex_int) => {
+                        let hex = hex_int.children
+                            .iter()
+                            .map(|hex| Ident::from_node(db, file, hex.deref()).unwrap())
+                            .collect::<Vec<_>>();
+                        Literal::BitString(BitString::Hex(Ident::join(db, &hex)))
+                    },
+                    ast::generated::BinaryInt_HexInt_OctalInt_UnsignedInt::OctalInt(octal_int) => {
+                        let octal = octal_int.children
+                            .iter()
+                            .map(|octal| Ident::from_node(db, file, octal.deref()).unwrap())
+                            .collect::<Vec<_>>();
+                        Literal::BitString(BitString::Octal(Ident::join(db, &octal)))
+                    },
+                    ast::generated::BinaryInt_HexInt_OctalInt_UnsignedInt::UnsignedInt(unsigned_int) => {
+                        let unsigned = unsigned_int.children
+                            .iter()
+                            .map(|unsigned| Ident::from_node(db, file, unsigned.deref()).unwrap())
+                            .collect::<Vec<_>>();
+                        Literal::BitString(BitString::Unsigned(Ident::join(db, &unsigned)))
+                    },
+                };
+
+            },
+            Constant::CharLiteral(char_literal) => {
+                match char_literal.char.children.deref() {
+                    ast::generated::DByteCharStr_SByteCharStr::SByteCharStr(s_byte_char_str) => {
+                        let char = Ident::from_node(db, file, s_byte_char_str.char.deref());
+                        //Literal::Char(Ident::join(db, &char));
+                    },
+                    ast::generated::DByteCharStr_SByteCharStr::DByteCharStr(d_byte_char_str) => {
+                        let char = Ident::from_node(db, file, d_byte_char_str.char.deref());
+                        //Literal::Char(Ident::join(db, &char));
+                    },
+                }
+            },
+            Constant::NumericLiteral(numeric_literal) => {
+                
+            },
+            Constant::TimeLiteral(time_literal) => todo!(),
+        }
+    }
+}
+
+impl<'db> ToVariable<'db> for ast::generated::VarDeclInit {
+    fn to_variables(&self, db: &'db dyn BaseDatabase, file: File) -> Variable<'db> {
+        match self.init.deref() {
+            ast::generated::VarDeclKind::ArraySpecInit(array) => todo!(),
+            ast::generated::VarDeclKind::StrVarDecl(_) => todo!(),
+            ast::generated::VarDeclKind::SimpleVarDecl(_) => todo!(),
+        }
+    }
 }
 
 impl<'db> ParseVariable<'db> for ast::generated::FuncDecl {
-    fn parse(&self, _db: &'db dyn BaseDatabase, _file: File) -> Vec<Variable<'db>> {
-        type VariableDecl = ast::generated::FuncVarDecls_IoVarDecls_TempVarDecls;
-        self.variables.iter().for_each(|variable| {
-            match variable.deref() {
-                VariableDecl::FuncVarDecls(variable) => {
-                    match variable.children.deref() {
-                        ast::generated::ExternalVarDecls_VarDecls::VarDecls(variable) => {
-                            variable.children.iter().for_each(|variable| {
+    fn parse_variables(&self, db: &'db dyn BaseDatabase, file: File) -> Vec<Variable<'db>> {
+        let mut intput_variables = vec![];
+        self.variables
+            .iter()
+            .for_each(|variable| match variable.deref() {
+                FuncVariables::InputDecls(variable) => {
+                    variable
+                        .children
+                        .iter()
+                        .for_each(|variable| match variable.deref() {
+                            ast::generated::InputVarKind::ArrayConformDecl(array_conform_decl) => {
+                                array_conform_decl.variables.children.iter().for_each(|variable| {
+                                    intput_variables.push(Variable::from(
+                                        db,
+                                        variable.get_text(file.document(db).as_bytes()).unwrap(),
+                                        VariableKind::Array(todo!()),
+                                    ));
+                                });
+                            }
+                            ast::generated::InputVarKind::EdgeDecl(edge_decl) => {
+                                edge_decl.variables.children.iter().for_each(|variable| {
+                                    intput_variables.push(Variable::from(
+                                        db,
+                                        variable.get_text(file.document(db).as_bytes()).unwrap(),
+                                        VariableKind::Edge
+                                    ));
+                                });
+                            }
+                            ast::generated::InputVarKind::VarDeclInit(var_decl_init) => {
+                                let kind = match var_decl_init.init.deref() {
+                                    ast::generated::VarDeclKind::ArraySpecInit(_) => VariableKind::Array(todo!()),
+                                    ast::generated::VarDeclKind::StrVarDecl(_) => VariableKind::Primitive(PrimitiveKind::String),
+                                    ast::generated::VarDeclKind::SimpleVarDecl(_) => {
+                                        todo!()
+                                    }
+                                };
                                 
-                            });
-                        }
-                        ast::generated::ExternalVarDecls_VarDecls::ExternalVarDecls(variable) => {
-                            variable.children.iter().for_each(|variable| {
                                 
-                            });
-                        }
-                    }
+                                var_decl_init
+                                    .variables
+                                    .children
+                                    .iter()
+                                    .for_each(|variable| {
+                                        intput_variables.push(Variable::from(
+                                            db,
+                                            variable.get_text(file.document(db).as_bytes()).unwrap(),
+                                            VariableKind::Primitive(PrimitiveKind::Bool),
+                                        ));
+                                    });
+                            }
+                        });
                 }
-                VariableDecl::IoVarDecls(variable) => {
-                    match variable.children.deref() {
-                        ast::generated::InOutDecls_InputDecls_OutputDecls::InputDecls(variable) => {
-                            variable.children.iter().for_each(|variable| {
+                FuncVariables::OutputDecls(variable) => {}
+                FuncVariables::InOutDecls(variable) => {}
+                FuncVariables::ExternalVarDecls(variable) => {}
+                FuncVariables::TempVarDecls(variable) => {}
+                FuncVariables::VarDecls(variable) => {}
+            });
 
-                            });
-                        }
-                        ast::generated::InOutDecls_InputDecls_OutputDecls::InOutDecls(variable) => {
-                            variable.children.iter().for_each(|variable| {
-                                
-                            });
-                        }
-                        ast::generated::InOutDecls_InputDecls_OutputDecls::OutputDecls(variable) => {
-                            variable.children.iter().for_each(|variable| {
-                                
-                            });
-                        }
-                    }
-                }
-                VariableDecl::TempVarDecls(variable) => {
-                    variable.children.iter().for_each(|variable| {
-                        match variable.deref() {
-                            ast::generated::InterfaceVarDecl_RefVarDecl_VarDecl::InterfaceVarDecl(variable) => {
-                                variable.children.iter().for_each(|variable| {
-                                
-                                });
-                            }
-                            ast::generated::InterfaceVarDecl_RefVarDecl_VarDecl::RefVarDecl(variable) => {
-                                variable.children.iter().for_each(|variable| {
-                                
-                                });
-                            }
-                            ast::generated::InterfaceVarDecl_RefVarDecl_VarDecl::VarDecl(variable) => {
-                                variable.children.iter().for_each(|variable| {
-                                
-                                });
-                            }
-                        }
-                    });
-                }
-            }
-        });
-        
-        vec![]
+        intput_variables
     }
 }
