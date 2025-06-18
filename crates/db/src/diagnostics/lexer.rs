1c001d3f960b6b20abb5fcb10435bea75c1cfc9c
adclz <clauzeladrien@gmail.com>
2025-06-18 15:48:02 +0200

    feat(diagnostics): reserved keywords error message

--- crates/db/src/diagnostics/lexer.rs
+++ crates/db/src/diagnostics/lexer.rs
@@ -1,10 +1,27 @@
+
 use std::collections::HashMap;
 
 use auto_lsp::core::errors::{LexerError, ParseError, ParseErrorAccumulator};
 use auto_lsp::lsp_types::{DiagnosticRelatedInformation, WorkspaceEdit};
+use phf::phf_set;
 
 use crate::diagnostics::diagnostic_builder::{action, diag, edit};
 use crate::diagnostics::IdeDiagnostic;
+
+static KEYWORDS: phf::Set<&'static str> = phf_set! {
+    "PROGRAM", "END_PROGRAM",
+    "CONFIGURATION", "END_CONFIGURATION",
+    "RESOURCE", "END_RESOURCE",
+    "NAMESPACE", "END_NAMESPACE",
+    "USING",
+    "CLASS", "END_CLASS",
+    "INTERFACE", "END_INTERFACE",
+    "FUNCTION", "END_FUNCTION",
+    "FUNCTION_BLOCK", "END_FUNCTION_BLOCK",
+    "TYPE", "END_TYPE",
+    "VAR", "END_VAR",
+    "VAR_INPUT",
+};
 
 pub fn add_fixes_to_parse_errors(
     db: &dyn auto_lsp::default::db::BaseDatabase,
@@ -91,6 +108,20 @@ pub fn add_fixes_to_parse_errors(
                             .call(),
                     );
                     diagnostic
+                } else if KEYWORDS.contains(affected.split_whitespace().next().unwrap_or("")) {
+                    diag()
+                        .range(range.into())
+                        .message(format!("{} is a reserved keyword that is not valid in this context", affected.split_whitespace().next().unwrap_or("")))
+                        .source("IEC".into())
+                        .severity(auto_lsp::lsp_types::DiagnosticSeverity::ERROR)
+                        .related_information(vec![DiagnosticRelatedInformation {
+                            location: auto_lsp::lsp_types::Location {
+                                uri: file.url(db).clone(),
+                                range: range.into(),
+                            },
+                            message: format!("help: remove '{}'", affected.split_whitespace().next().unwrap_or("")).into(),
+                        }])
+                        .call()
                 } else {
                     (*error).into()
                 }
