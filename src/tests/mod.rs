e59bfc9bb440d467138af02cc9f479f27aa2bffd
adclz <clauzeladrien@gmail.com>
2025-09-10 17:12:50 +0200

    refactor(test): split up lsp and semantic tests

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,8 +1,3 @@
-pub mod comment_index;
-pub mod lexer;
-pub mod literals;
-pub mod scopes;
-pub mod assignment_errors;
-pub mod inheritance_errors;
-pub mod syntax_errors;
 pub mod utils;
+pub mod lsp;
+pub mod semantics;
---
+++ src/tests/semantics/mod.rs
@@ -0,0 +1,5 @@
+pub mod assignment;
+pub mod inheritance;
+pub mod literals;
+pub mod scopes;
+pub mod syntax;
