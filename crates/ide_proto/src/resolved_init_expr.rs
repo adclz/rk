fa3d4de846a7475c5e8130c55046abedd266e7f9
adclz <clauzeladrien@gmail.com>
2025-10-06 15:18:40 +0200

    feat(ide_proto): add inlay hints and type resolution for struct elements in ResolvedInitExpr

--- crates/ide_proto/src/resolved_init_expr.rs
+++ crates/ide_proto/src/resolved_init_expr.rs
@@ -1,15 +1,35 @@
-use auto_lsp::{default::db::BaseDatabase, lsp_types::request::GotoDeclarationResponse};
+use auto_lsp::{default::db::BaseDatabase, lsp_types::{request::GotoDeclarationResponse, InlayHint, InlayHintKind, InlayHintLabel, Position}};
 use hir::{
     hir_def::expressions::expression::InitExprKind,
-    hir_ty::{expr_resolver::resolve_expr, init_expr_resolver::{ResolvedInitExpr, ResolvedInitExprKind}},
+    hir_ty::{
+        expr_resolver::resolve_expr,
+        init_expr_resolver::{ResolvedInitExpr, ResolvedInitExprKind}, ty::Ty,
+    }, HirNodeInfo, TypeInfo,
 };
 
 use crate::ToProtocol;
 
 impl<'db> ToProtocol<'db> for ResolvedInitExpr<'db> {
+    fn inlay_hint(&'db self, db: &'db dyn BaseDatabase) -> Option<InlayHint> {
+        Some(InlayHint {
+            position: get_expr_inlay_hint_position(db, self)?,
+            label: InlayHintLabel::String(match get_expr_ty(db, self) {
+                Some(ty) => format!(": {}", ty.type_name(db)),
+                None => return None,
+            }),
+            kind: Some(InlayHintKind::TYPE),
+            padding_left: Some(false),
+            padding_right: Some(false),
+            text_edits: None,
+            tooltip: None,
+            data: None,
+        })
+    }
+
     fn declaration(&'db self, db: &'db dyn BaseDatabase) -> Option<GotoDeclarationResponse> {
         match self.kind(db) {
             ResolvedInitExprKind::ConstantExpr(expr) => expr.declaration(db),
+            ResolvedInitExprKind::StructElement { field, value } => field.declaration(db),
             _ => None,
         }
     }
@@ -20,6 +40,7 @@ impl<'db> ToProtocol<'db> for ResolvedInitExpr<'db> {
     ) -> Option<auto_lsp::lsp_types::GotoDefinitionResponse> {
         match self.kind(db) {
             ResolvedInitExprKind::ConstantExpr(expr) => expr.definition(db),
+            ResolvedInitExprKind::StructElement { field, value } => field.definition(db),
             _ => None,
         }
     }
@@ -31,7 +52,27 @@ impl<'db> ToProtocol<'db> for ResolvedInitExpr<'db> {
     ) -> Option<auto_lsp::lsp_types::Hover> {
         match self.kind(db) {
             ResolvedInitExprKind::ConstantExpr(expr) => expr.hover(db, offset),
+            ResolvedInitExprKind::StructElement { field, value } => field.hover(db, offset),
             _ => None,
         }
+    }
+}
+
+pub fn get_expr_inlay_hint_position(
+    db: &dyn BaseDatabase,
+    param: &ResolvedInitExpr,
+) -> Option<Position> {
+    match param.kind(db) {
+        ResolvedInitExprKind::StructElement { field, .. } => {
+            Some(field.get_span(db).lsp().end)
+        },
+        _ => None,
+    }
+}
+
+pub fn get_expr_ty<'db>(db: &'db dyn BaseDatabase, param: &'db ResolvedInitExpr) -> Option<Ty<'db>> {
+    match param.kind(db) {
+        ResolvedInitExprKind::StructElement { field, .. } => field.ty(db).ok(),
+        _ => None,
     }
 }
