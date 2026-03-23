a88f6bb1b181a6a175f0f31f582304b7cd6ebb6f
adclz <clauzeladrien@gmail.com>
2026-03-23 02:32:13 +0100

    feat(ide_proto): add command to run tests with task execution in VSCode

--- crates/ide_proto/src/handlers/code_lens.rs
+++ crates/ide_proto/src/handlers/code_lens.rs
@@ -1,6 +1,7 @@
 use auto_lsp::lsp_types::{CodeLens, Command};
 use db::WorkspaceDataBase;
 use hir::{HasName, HirNodeInfo, hir_def::hir_node::HirNode, hir_def::pous::pou::Pou};
+use hir::hir_ty::ty::Type;
 use serde_json::to_value;
 
 use crate::handlers::{CodeLensHandler, implementation::find_all_implementations};
@@ -9,11 +10,14 @@ impl<'db> CodeLensHandler<'db> for HirNode<'db> {
     fn code_lens(&self, db: &'db dyn WorkspaceDataBase) -> Option<CodeLens> {
         match self {
             HirNode::PouDecl(pou) => pou.code_lens(db),
-            HirNode::Program(prog) if prog.is_test(db) => Some(test_code_lens(
-                prog.get_span(db).lsp(),
-                prog.get_scope_id(db).file(db).url(db).as_str(),
-                &prog.name(db).text(db),
-            )),
+            HirNode::Program(prog) if prog.is_test(db) => {
+                let qualified = Type::Program(*prog).qualified_path(db);
+                Some(test_code_lens(
+                    prog.get_span(db).lsp(),
+                    prog.get_scope_id(db).file(db).url(db).as_str(),
+                    &qualified,
+                ))
+            }
             _ => None,
         }
     }
@@ -45,11 +49,14 @@ impl<'db> CodeLensHandler<'db> for Pou<'db> {
                     })
                 }
             }
-            Pou::Function(f) if f.is_test(db) => Some(test_code_lens(
-                self.get_span(db).lsp_with_enc(self.get_scope_id(db).file(db).document(db)).unwrap(),
-                self.get_scope_id(db).file(db).url(db).as_str(),
-                &self.get_name_ident(db).text(db),
-            )),
+            Pou::Function(f) if f.is_test(db) => {
+                let qualified = Type::new_pou(db, *self).qualified_path(db);
+                Some(test_code_lens(
+                    self.get_span(db).lsp_with_enc(self.get_scope_id(db).file(db).document(db)).unwrap(),
+                    self.get_scope_id(db).file(db).url(db).as_str(),
+                    &qualified,
+                ))
+            }
             _ => None,
         }
     }
